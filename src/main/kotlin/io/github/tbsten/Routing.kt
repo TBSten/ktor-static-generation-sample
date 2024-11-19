7acb5c2e6ad71d5fe91735c6fcf430bf5bd49ae0
TBSten <programmingCafeteria@gmail.com>
2024-11-20 04:14:49 +0900

    staticGeneration を使用する例

--- src/main/kotlin/io/github/tbsten/Routing.kt
+++ src/main/kotlin/io/github/tbsten/Routing.kt
@@ -3,11 +3,21 @@ package io.github.tbsten
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.flow.asFlow
+import me.tbsten.ktor.staticGeneration.staticGeneration
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
+        staticGeneration("/") {
             call.respondText("Hello World!")
+        }
+
+        staticGeneration(
+            "/blog/{blogId}",
+            staticPaths = { listOf("/blog/1", "/blog/2").asFlow() },
+        ) {
+            val blogId = call.parameters["blogId"]
+            call.respondText("Hello blog id=$blogId")
         }
     }
 }
