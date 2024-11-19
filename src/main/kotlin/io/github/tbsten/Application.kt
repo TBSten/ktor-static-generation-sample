1c9aed619f5952e5058c6e9f65afcd9f2e0f06c9
TBSten <programmingCafeteria@gmail.com>
2024-11-20 04:07:20 +0900

    StaticGeneration のセットアップ

--- src/main/kotlin/io/github/tbsten/Application.kt
+++ src/main/kotlin/io/github/tbsten/Application.kt
@@ -1,11 +1,13 @@
 package io.github.tbsten
 
 import io.ktor.server.application.*
+import me.tbsten.ktor.staticGeneration.StaticGeneration
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(StaticGeneration)
     configureRouting()
 }
