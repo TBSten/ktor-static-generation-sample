c2852ad402ae1e21ed0ad2c0827be62f59c2578b
TBSten <programmingCafeteria@gmail.com>
2024-11-20 05:10:22 +0900

    /style.css のレスポンスをcssで構築

--- src/main/kotlin/io/github/tbsten/Routing.kt
+++ src/main/kotlin/io/github/tbsten/Routing.kt
@@ -1,21 +1,39 @@
 package io.github.tbsten
 
+import io.github.tbsten.util.respondCss
 import io.ktor.server.application.*
 import io.ktor.server.html.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.flow.asFlow
-import kotlinx.html.FlowOrHeadingContent
-import kotlinx.html.body
-import kotlinx.html.h1
+import kotlinx.css.*
+import kotlinx.html.*
 import me.tbsten.ktor.staticGeneration.staticGeneration
 
 fun Application.configureRouting() {
     routing {
         staticGeneration("/") {
             call.respondHtml {
+                head {
+                    link {
+                        rel = "stylesheet"
+                        href = "./style.css"
+                    }
+                }
                 body {
                     h1 { +"Hello TBSten blog" }
+                }
+            }
+        }
+
+        staticGeneration("/style.css") {
+            call.respondCss {
+                body {
+                    fontSize = 16.px
+                }
+                h1 {
+                    backgroundColor = Color.red
+                    color = Color.white
+                    fontSize = 32.px
                 }
             }
         }
